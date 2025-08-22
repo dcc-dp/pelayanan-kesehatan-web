d87a7a415012644656f62259dcf28c7d07f64575
Pipiw <safira.syah86@gmail.com>
2025-08-23 01:02:58 +0800

    tes

--- src/app/api/users/[id]/route.js
+++ src/app/api/users/[id]/route.js
@@ -1,23 +1,23 @@
 import { NextResponse } from "next/server";
-import pool from "@/libs/mysql";
+import pool from "@/src/libs/mysql";
 
 export async function GET(request, { params }) {
-  const slug = params.slug; // user id
+  const userId = params.id; // user id
 
   try {
     const db = await pool.getConnection();
 
-    const query = "select * from account where id = ?";
-    const [rows] = await db.execute(query, [slug]);
+    const query = "select * from users where id = ?";
+    const [rows] = await db.execute(query, [userId]);
     db.release();
 
     return NextResponse.json(rows);
   } catch (error) {
     return NextResponse.json(
       {
-        error: error,
+        error: error. message,
       },
-      { status: 500 }
+      { error: error.message, status: 500 }
     );
   }
 }
